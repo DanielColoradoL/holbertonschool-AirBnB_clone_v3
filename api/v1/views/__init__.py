b431579b10e98ae8c7fca4156319a4fabe4d31ba
Zona <echyth@yahoo.com>
2024-04-20 19:55:35 +1000

    fix (task 7): updates for failing tests (#15)
    * fix(task 1): correct style errors in console.py
    
    * fix(task 6): update not_found route response status
    
    * fix(task 6): update not_found route response status
    
    * feat(task 7): implement states routes
    
    * feat(task 7): implement states routes
    
    * feat(task 7): implement states routes
    
    * feat(task 7): implement states routes
    
    * fix(task 7): updates for failing test
    
    * fix(task 7): updates for failing test
    
    * fix(task 7): updates for failing test
    
    * fix(task 7): updates for failing test
    
    * fix(task 7): updates for failing test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
+
 
 # defining a blueprint
 app_views = Blueprint(
     "app_views",
     __name__,
     url_prefix="/api/v1")
+
+
+from api.v1.views.index import *
+from api.v1.views.states import *
